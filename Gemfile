edd82d5edbee15bcdfab94b30d1923c77abb1b3a
maehachi08 <maehachi08@gmail.com>
2025-09-28 10:27:44 +0900

    fix: coverage reports

--- Gemfile
+++ Gemfile
@@ -64,6 +64,8 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem 'simplecov'
+  gem 'simplecov-cobertura'
 end
 
 # GoosetuneTV Custom
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,44 @@
 ENV["RAILS_ENV"] ||= "test"
+
+# SimpleCov configuration - must be loaded before application code
+require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.start 'rails' do
+  # Add coverage for all application code
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Libraries', 'lib'
+
+  # Configure formatters for both HTML and Cobertura XML reports
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::CoberturaFormatter
+  ])
+
+  # Set minimum coverage threshold
+  minimum_coverage 80
+
+  # Refuse to merge results that are more than 10 minutes old
+  merge_timeout 600
+
+  # Coverage directory
+  coverage_dir 'coverage'
+
+  # Skip files that shouldn't be covered
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/db/'
+  add_filter 'app/channels/application_cable/'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+
+  # Track files even if they're never loaded
+  track_files '{app,lib}/**/*.rb'
+end
+
 require_relative "../config/environment"
 require "rails/test_help"
 
